9faa59fa95f167b2712366ca3f1b38de6ef608d3
Martin Bergemann <martin.bergemann@monash.edu>
2017-11-10 15:49:09 +1100

    Added python module to fix problems with missing data at the beginning of a timeseries

---
+++ process_rain/correct_ts.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2.7
+
+from netCDF4 import Dataset as nc
+from netCDF4 import date2num
+import numpy as np
+import sys
+from datetime import datetime
+try:
+    infile=sys.argv[1]
+except IndexError:
+    sys.exit('Error: \n Usage: %s infile.nc' %sys.argv[0])
+
+with nc(infile,'a') as f:
+  for v in ('rain_rate','rain_rate_pdf'):
+    try:
+      f.variables[v][:]=f.variables[v][:].filled(0.0)
+    except AttributeError:
+      pass
